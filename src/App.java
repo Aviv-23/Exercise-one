1bc253ad68ac565ddc18e1235a404854bd445240
Aviv-23 <avivneiger@gmail.com>
2026-01-16 12:28:59 -0500

    done

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 public class App {
+	
 	public static void main(String[] args){
 		int[][] a = {
 			{1,2,3},
@@ -9,7 +10,13 @@ public class App {
 	}
 	
 	public static int sum2D(int[][] nums){
-		return -1;
+		int sum=0;
+		for(int i=0; i<nums.length; i++){
+			for(int j=0; j<nums[i].length; j++){
+				sum+=nums[i][j];
+			}
+		}
+		return sum;
 	}
 	
 }
